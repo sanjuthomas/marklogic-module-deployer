fc5ff7bfe56f179cfea68c49645dc2dc33d8a028
sanjuthomas <sanju@sanju.org>
2016-10-11 22:31:25 -0400

    adding transform deployer

--- src/main/java/org/sanju/ml/deployer/TransformDeployer.java
+++ src/main/java/org/sanju/ml/deployer/TransformDeployer.java
@@ -2,28 +2,40 @@ package org.sanju.ml.deployer;
 
 import java.io.File;
 
+import org.apache.commons.io.FilenameUtils;
+import org.sanju.ml.payload.PayloadHelper.ContentType;
 import org.sanju.ml.payload.TransformsPayload;
 
 import com.marklogic.client.DatabaseClient;
 import com.marklogic.client.admin.TransformExtensionsManager;
+import com.marklogic.client.io.FileHandle;
 
 /**
  * 
  * @author Sanju Thomas
  *
  */
-public class TransformDeployer implements ModuleDeployer<TransformsPayload>{
-	
+public class TransformDeployer implements ModuleDeployer<TransformsPayload> {
+
 	final DatabaseClient databaseClient;
-	
-	public TransformDeployer(final DatabaseClient databaseClient){
+
+	public TransformDeployer(final DatabaseClient databaseClient) {
 		this.databaseClient = databaseClient;
 	}
 
 	@Override
 	public void deploy(TransformsPayload t) {
-		final TransformExtensionsManager transformExtensionsManager = this.databaseClient.newServerConfigManager().newTransformExtensionsManager();
+		final TransformExtensionsManager tem = this.databaseClient.newServerConfigManager().newTransformExtensionsManager();
 		final File file = t.getFile();
+		final String contentType = t.getContentType();
+
+		if (ContentType.XLS.getType().equalsIgnoreCase(contentType)) {
+			tem.writeXSLTransform(FilenameUtils.removeExtension(file.getName()), new FileHandle(file));
+		}else if(ContentType.XQY.getType().equalsIgnoreCase(contentType)){
+			tem.writeXQueryTransform(FilenameUtils.removeExtension(file.getName()), new FileHandle(file));
+		}else if(ContentType.SJS.getType().equalsIgnoreCase(contentType)){
+			tem.writeJavascriptTransform(FilenameUtils.removeExtension(file.getName()), new FileHandle(file));
+		}
 	}
 
 }
--- src/main/java/org/sanju/ml/payload/PayloadHelper.java
+++ src/main/java/org/sanju/ml/payload/PayloadHelper.java
@@ -15,15 +15,33 @@ import com.marklogic.client.admin.ExtensionMetadata.ScriptLanguage;
  */
 public class PayloadHelper {
 
+	
 	private final static Map<String, ScriptLanguage> fileExtensionLanguageMap = new HashMap<>();
 	private final static Map<String, String> fileExtensionContentTypeMap = new HashMap<>();
 
 	static{
 		fileExtensionLanguageMap.put("sjs", ScriptLanguage.JAVASCRIPT);
 		fileExtensionLanguageMap.put("xqy", ScriptLanguage.XQUERY);
-		fileExtensionContentTypeMap.put("xls", "application/xslt+xml");
-		fileExtensionContentTypeMap.put("xqy", "application/xquery");;
-		fileExtensionContentTypeMap.put("sjs", "application/javascript");		
+		fileExtensionContentTypeMap.put("xls", ContentType.XLS.getType());
+		fileExtensionContentTypeMap.put("xqy", ContentType.XQY.getType());
+		fileExtensionContentTypeMap.put("sjs", ContentType.SJS.getType());		
+	}
+	
+	public enum ContentType{
+		
+		XLS("application/xslt+xml"),
+		XQY("application/xslt+xml"),
+		SJS("application/xslt+xml");
+	
+		private String type;
+		
+		ContentType(String type){
+			this.type = type;
+		}
+		
+		public String getType(){
+			return this.type;
+		}
 	}
 
 	public static ScriptLanguage getScriptingLanguage(final File file) {
