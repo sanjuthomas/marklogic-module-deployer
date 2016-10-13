6e6603e64fdf7fd6ec48606ac769a461ab761969
sthomas <sanju.thomas@marklogic.com>
2016-10-13 11:53:24 -0400

    adding tarnsform deployer

--- src/test/java/org/sanju/ml/deployer/TestQueryOptionDeployer.java
+++ src/test/java/org/sanju/ml/deployer/TestQueryOptionDeployer.java
@@ -1,7 +1,33 @@
 package org.sanju.ml.deployer;
 
+import java.io.File;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.sanju.ml.MLClientFactory;
+import org.sanju.ml.payload.QueryOptionPayload;
+
+/**
+ *
+ * @author Sanju Thomas
+ *
+ */
 public class TestQueryOptionDeployer extends AbstractTest{
 
+	private ModuleDeployer<QueryOptionPayload> mlModuleDeployer;
+	private QueryOptionPayload queryOptionPayload;
 
 
+	@Override
+	@Before
+	public void setup(){
+		super.setup();
+		this.mlModuleDeployer = new QueryOptionDeployer(MLClientFactory.getClient(this.mlApplicationServer));
+		this.queryOptionPayload = new QueryOptionPayload(new File("src/test/resources/query-options/test-query-option.xml"));
+	}
+
+	@Test
+	public void shouldLoadQueryOptions(){
+		this.mlModuleDeployer.deploy(this.queryOptionPayload);
+	}
 }
