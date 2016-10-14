553d6b8bb1ac5ab6aea5e35b5178b622e246f5aa
sthomas <sanju.thomas@marklogic.com>
2016-10-14 11:49:54 -0400

    adding loader

---
+++ src/main/java/org/sanju/ml/loader/ModulesLoader.java
@@ -0,0 +1,18 @@
+package org.sanju.ml.loader;
+
+import java.util.Properties;
+
+/**
+ *
+ * @author Sanju Thomas
+ *
+ */
+public class ModulesLoader implements Loader{
+
+	@Override
+	public void load(final Properties properties) {
+
+
+	}
+
+}
