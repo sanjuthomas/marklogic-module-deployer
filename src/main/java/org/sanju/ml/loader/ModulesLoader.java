07e9e9fad56101d7d15683c42f184777adc3f993
sthomas <sanju.thomas@marklogic.com>
2016-10-14 15:57:10 -0400

    somr emore

--- src/main/java/org/sanju/ml/loader/ModulesLoader.java
+++ src/main/java/org/sanju/ml/loader/ModulesLoader.java
@@ -9,10 +9,47 @@ import java.util.Properties;
  */
 public class ModulesLoader implements Loader{
 
-	@Override
-	public void load(final Properties properties) {
+	enum ModuleTypes{
 
+		REST_EXT("rest.ext.deployer", "rest.ext.payload", "rest.ext.location"),
+		OPTIONS("rest.options.deployer", "rest.options.payload", "rest.options.location"),
+		TRANSFORMS("rest.transform.deployer", "rest.transform.payload", "rest.transform.location"),
+		LIBRARIES("rest.libraries.deployer", "rest.libraries.payload", "rest.libraries.location");
+
+		private final String deployerClass;
+		private final String payloadClass;
+		private final String sourceLocation;
+
+		ModuleTypes(final String deployer, final String payload, final String location){
+			this.deployerClass = deployer;
+			this.payloadClass = payload;
+			this.sourceLocation = location;
+		}
+
+		public String getDeployerClass() {
+			return this.deployerClass;
+		}
+
+		public String getPayloadClass() {
+			return this.payloadClass;
+		}
+
+		public String getSourceLocation() {
+			return this.sourceLocation;
+		}
 
 	}
 
+	@Override
+	public void load(final Properties properties) {
+
+		final ModuleTypes[] types = ModuleTypes.values();
+		for(final ModuleTypes type : types){
+
+
+		}
+	}
+
 }
+
+
